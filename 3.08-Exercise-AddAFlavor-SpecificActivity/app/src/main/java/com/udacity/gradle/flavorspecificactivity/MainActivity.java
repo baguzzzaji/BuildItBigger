7004c503af80cbb3afe7ad94f791327f200da01c
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-09-28 23:34:55 +0700

    Created java library for telling a joke

--- 3.08-Exercise-AddAFlavor-SpecificActivity/app/src/main/java/com/udacity/gradle/flavorspecificactivity/MainActivity.java
+++ 3.08-Exercise-AddAFlavor-SpecificActivity/app/src/main/java/com/udacity/gradle/flavorspecificactivity/MainActivity.java
@@ -1,11 +1,13 @@
 package com.udacity.gradle.flavorspecificactivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -15,6 +17,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button paid = (Button) findViewById(R.id.button_paid);
+        paid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PaidActivity.class));
+            }
+        });
     }
 
 
